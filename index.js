b2b682d984b047af16ccbc3328a82559c3437f99
Jakob Ornysh <jornysh@gmail.com>
2025-03-04 18:02:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const {readLine} = require('./console');
 
 const files = getFiles();
 
+let todos = [];
+
 console.log('Please, write your command!');
 readLine(processCommand);
 
@@ -12,9 +14,35 @@ function getFiles() {
 }
 
 function processCommand(command) {
+    if (command.startsWith('user ')) {
+        const username = command.slice(5).toLowerCase();
+        todos = parseTodo();
+        const userTodos = todos.filter(todo => {
+            const match = todo.match(/^([^;]+)/);
+            return match && match[1].trim().toLowerCase() === username;
+        });
+        for (let todo of userTodos) {
+            console.log(todo);
+        }
+        return;
+    }
+
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            todos = parseTodo();
+            for (let todo of todos) {
+                console.log(todo);
+            }
+            break;
+        case 'important':
+            todos = parseTodo();
+            const importantTodos = todos.filter(todo => todo.includes('!'));
+            for (let todo of importantTodos) {
+                console.log(todo);
+            }
             break;
         default:
             console.log('wrong command');
@@ -22,4 +50,18 @@ function processCommand(command) {
     }
 }
 
-// TODO you can do it!
+// TODO Jakob; 2025-03-02; Сделать практику!
+function parseTodo() {
+    const comments = [];
+    for (const file of files) {
+        const lines = file.split('\r\n');
+        for (const line of lines) {
+            const todoIndex = line.indexOf('// TODO');
+            if (todoIndex !== -1 && !line.includes('\'// TODO')) {
+                const todoText = line.slice(todoIndex + 7).trim();
+                comments.push(todoText);
+            }
+        }
+    }
+    return comments;
+}
